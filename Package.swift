cccd426d79d6d5f2f9225545890558ba68d954c7
Prageeth Monasha <spmonasha@gmail.com>
2026-03-28 17:50:41 +0100

    [cleanup]

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "EdgeMetalServer",
-    platforms: [.macOS(.v14)],   // Metal read_write textures, structured concurrency
+    platforms: [.macOS(.v14)],  // Metal read_write textures, structured concurrency
     targets: [
         .executableTarget(
             name: "EdgeMetalServer",
